bf373faa0bb9a43226cdf619c4759cf822ce521d
dalilaorbaj <dali.orbaj24@gmail.com>
2022-08-01 17:40:33 -0300

    Mi primera app de React Native

---
+++ App.js
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Modal from "react-native-modal";
+import React, { useState } from "react";
+
+
+
+export default function App() {
+
+const [visible, setVisible] = useState(false);
+
+const toggleModal = () => {
+  setVisible(!visible);
+};
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Button title='Boton' onPress={()=>toggleModal()}></Button>
+      <Modal isVisible={visible}>
+        <Text style={styles.text}>Jeje modal</Text>
+        <Button title='Chau modal' onPress={()=>toggleModal()}></Button>
+      </Modal>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text:{
+    color: '#ffff', 
+  
+  }
+});
